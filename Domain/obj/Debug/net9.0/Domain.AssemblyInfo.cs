7a9d5a6388e46767bd3802dfa587c21660a3729f
thanhdat <thanhdatho0@gmail.com>
2025-05-17 03:20:16 +0700

    f

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f593890cdffae99def3f837751daf723763661e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304955749aa778067a7ddd7edee9919d4dec8534")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
